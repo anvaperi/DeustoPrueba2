51ebdeff23414d0ebec36a8f953cec1bd00c9068
anvaperi <donantonio5314@gmail.com>
2025-08-08 09:50:03 +0200

    created promise returning function

--- js/main.js
+++ js/main.js
@@ -4,12 +4,36 @@ class Student {
   constructor(forename, surname, score) {
     this.forename = forename;
     this.surname = surname;
-    this.score = score;
+    this.setScore(score);
   }
   setScore (newScore) {
-    this.score = newScore;
+    this.score = parseInt(newScore);
   }
   getPassStatus() {
     return this.score >= 5 ? "Apto" : "No Apto";
   }
 }
+
+function createStudentPromise() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const student = new Student(
+        document.getElementById('forename').value, 
+        document.getElementById('surname').value, 
+        document.getElementById('score').value
+        ); 
+      if (!student.forename || !student.surname || isNaN(student.score)) {
+        reject("Error: Datos del formulario incompletos o incorrectos.");
+      }
+      resolve(`${student.forename} ${student.surname}: ${student.getPassStatus()}`);
+    }, 2000);
+  });
+}
+
+createStudentPromise()
+  .then((value) => {
+    console.log(value);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
